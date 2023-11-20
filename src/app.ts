339b3031899e9531b498ea9fc92114eb833ea2b1
Kar200707 <harutyunyan2007karen@gmail.com>
2023-11-20 17:08:33 +0400

    set origin use *

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import router from "./routers/categoryRouter";
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const runServer = require('../src/server/server');
 const connectDB = require('../src/DB/dbConnection');
+
+app.use(cors({
+    origin: "*",
+}));
 
 connectDB().then((param):void =>{
     param ? runServer(app) : console.log('server stoped');
